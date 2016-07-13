bbea53805d966faf864605c877bd6583ed1d20cd
Awantura <kalafior.seler@gmail.com>
2016-07-13 23:34:19 +0200

    Update basket_content.rb

--- basket_content.rb
+++ basket_content.rb
@@ -13,22 +13,19 @@ module Shop
 		@price = price
 	end
   end 
-
- end
+  
+  def generate_and_print
+    products << Product.new({id: 1, name: "Ball", price: 12.00})
+    products << Product.new({id: 2, name: "Dress", price: 375.00})
+    products << Product.new({id: 3, name: "Pancake powder", price: 23.70})
+    
+    products.each do 
+    	puts "#{@name} - Price:#{@price}"
+  end
+end
 
  private
 
  def next_id
  	@@id += 1
  end
-
-product1 = Product.new({id: 1, name: "Ball", price: 12.00})
-product2 = Product.new({id: 2, name: "Dress", price: 375.00})
-product3 = Product.new({id: 3, name: "Pancake powder", price: 23.70})
-
-
-
-
-basket = Basket.new
-
-p bakset.products
