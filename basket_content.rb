e653b8b8b8afd6b50194eb4d1e14b79a15b03fb5
Awantura <kalafior.seler@gmail.com>
2016-07-14 15:39:42 +0200

    Update basket_content.rb

--- basket_content.rb
+++ basket_content.rb
@@ -3,28 +3,28 @@ require_relative "./basket_content.rb"
 module Shop
 
   class Product
-	attr_reader :name, :price
+    attr_reader :name, :price
 
-	@@id = 0
+    @@id = 0
 
-	def initialize(name, price)
-		@id = next_id
-		@name = name
-		@price = price
+    def initialize(name, price)
+      @id = next_id
+      @name = name
+      @price = price
 	end
 	
-  	def generate_and_print
-    		products << Product.new({id: 1, name: "Ball", price: 12.00})
-    		products << Product.new({id: 2, name: "Dress", price: 375.00})
-    		products << Product.new({id: 3, name: "Pancake powder", price: 23.70})
+    def generate_and_print
+      products << Product.new({id: 1, name: "Ball", price: 12.00})
+      products << Product.new({id: 2, name: "Dress", price: 375.00})
+      products << Product.new({id: 3, name: "Pancake powder", price: 23.70})
     
-		products.each do 
-    	        	puts "#{@name} - Price:#{@price}"
-  	end
-  	private
-  	def next_id
- 		@@id += 1
-	end
+      products.each do 
+        puts "#{@name} - Price:#{@price}"
+      end
+      private
+      def next_id
+        @@id += 1
+      end
   end 
 end
 
